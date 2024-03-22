b187fd01bf7db864d2417912e3ff216639ae8908
Ayan Alam <160322944+AyanAlam02@users.noreply.github.com>
2024-03-22 22:06:55 +0500

    Completed Simple Calculator
    Completed the Calculator Project And Made The Npm Package Named (simple-calculator-ayan)

---
+++ index.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the Operators to perform calculation",
+    type: "list",
+    name: "operator",
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Multiplication",
+      "Division",
+      "Modulus",
+      "Exponential",
+    ],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else if (answer.operator === "Modulus") {
+  console.log(answer.firstNumber % answer.secondNumber);
+} else if (answer.operator === "Exponential") {
+  console.log(answer.firstNumber ** answer.secondNumber);
+} else {
+  console.log("Please, Select Valid Operator.");
+}
+console.log("Made By Ayan Alam");
